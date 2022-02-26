19020d9b4c3ebee0f0be5596c1d5616b8d7d1d50
Ashutosh Maurya <ashumaurya486@gmail.com>
2022-02-27 01:43:57 +0530

    showing data in card

--- src/component/shoping/Product.tsx
+++ src/component/shoping/Product.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Product.css";
+import { Card } from "@material-ui/core";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Col, Row } from "reactstrap";
+import { Share } from "@material-ui/icons";
 
 export interface Iproduct {
   id: number;
@@ -27,14 +35,6 @@ function Product(Iproduct: any) {
         console.log("error are", err);
       });
   };
-  // useEffect(() => {
-  //   getProductData();
-  // }, []);
-
-  // const getProduct = () => {
-  //  setProduct(response.data)
-  //   console.log("this is tet");
-  // };
 
   return (
     <>
@@ -47,7 +47,28 @@ function Product(Iproduct: any) {
         {product.map((comingData: any) => {
           return (
             <>
-              <div className="card" style={{ width: "18rem" }}>
+              <Row>
+                <Col md="4" sm="12">
+                  <Card style={{ maxWidth: 345 }}>
+                    <CardMedia component="img" image={comingData.image} />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {comingData.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {comingData.description}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">
+                        <Share />
+                      </Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Col>
+              </Row>
+              {/* <div className="card" style={{ width: "18rem" }}>
                 <img
                   src={comingData.image}
                   className="card-img-top"
@@ -58,8 +79,8 @@ function Product(Iproduct: any) {
                   <h5 className="card-title">{comingData.title}</h5>
                   <p className="card-text">{comingData.description}</p>
                   {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-                </div>
-              </div>
+              {/* </div> */}
+              {/* </div> */}
             </>
           );
         })}
